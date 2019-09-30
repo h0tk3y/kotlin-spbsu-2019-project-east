53a92649622133fa834ce2de846240a35e3a80a6
Evgeny Im <evgenyim6456@gmail.com>
2019-09-30 17:01:45 +0300

    add Chat structure

--- src/main/kotlin/ru/snailmail/backend/Chat.kt
+++ src/main/kotlin/ru/snailmail/backend/Chat.kt
@@ -1,4 +1,13 @@
 package ru.snailmail.backend
 
-class Chat {
+open class Chat (var name : String, val chatId : Int) {
+    var messages : List<Message> = emptyList()
+    var users : List<User> = emptyList()
+    open fun sendMessage(message: Message) { }
+    fun changeName (newName : String) {
+        name = newName
+    }
+    open fun searchMessage(s : String) { }
+    open fun printMessage (message : Message) {}
+
 }
