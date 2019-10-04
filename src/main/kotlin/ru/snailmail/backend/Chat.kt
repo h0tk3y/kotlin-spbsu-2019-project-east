260c473a30b03124c54d90205bb60542025d9273
Evgeny Im <evgenyim6456@gmail.com>
2019-10-04 12:22:26 +0300

    refactored Chat.kt

--- src/main/kotlin/ru/snailmail/backend/Chat.kt
+++ src/main/kotlin/ru/snailmail/backend/Chat.kt
@@ -1,13 +1,14 @@
 package ru.snailmail.backend
 
-open class Chat (var name : String, val chatId : Int) {
-    var messages : List<Message> = emptyList()
-    var users : List<User> = emptyList()
+open class Chat (var name : String) {
+    val chatID = UIDGenerator.generateID()
+    val messages = emptyList<Message>()
+    val users = emptyList<User>()
     open fun sendMessage(message: Message) { }
     fun changeName (newName : String) {
         name = newName
     }
     open fun searchMessage(s : String) { }
-    open fun printMessage (message : Message) {}
+    open fun printMessage (message : Message) { }
 
 }
