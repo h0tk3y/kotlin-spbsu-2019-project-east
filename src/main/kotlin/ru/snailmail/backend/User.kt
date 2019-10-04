feb31930fad58dec07bea296a91c9fc891b951e4
FadeevaKate <rediska0123@gmail.com>
2019-10-04 14:48:45 +0300

    Improved User class

--- src/main/kotlin/ru/snailmail/backend/User.kt
+++ src/main/kotlin/ru/snailmail/backend/User.kt
@@ -1,17 +1,21 @@
 package ru.snailmail.backend
 
-class User (var name : String) {
+class User(var name: String) {
     val userID = UIDGenerator.generateID()
     val chatList = mutableListOf<Chat>()
-    val userList = mutableListOf<User>() // saved
+    val contactsList = mutableListOf<Contact>()
 
-    fun changeName(newName : String) {
-        name = newName
+    fun changeName(newName : String) { name = newName }
+
+    inner class Contact {
+        val userID: Int = 0 // Id of the corresponding user
+        val prefferedName: String = name
+        val isBlocked: Boolean = false
     }
 
-    fun addChat (chat: Chat) {
+    fun addChat(chat: Chat) {
         if (chatList.contains(chat)) {
-            throw IllegalArgumentException("Already exist")
+            throw IllegalArgumentException("Already exists")
         }
         chatList.add(chat)
     }
@@ -22,4 +26,9 @@ class User (var name : String) {
         }
         chatList.remove(chat)
     }
+
+    fun AddContact(u: User): Nothing = TODO()
+    fun SetPrefferedName(id: Int, newPrefferedName: String): Nothing = TODO()
+    fun BlockUser(id: Int): Nothing = TODO()
+    fun UnblockUser(id: Int): Nothing = TODO()
 }
