0e192c89b04ea8514415d0f3977a6ba5b9bc6be0
Anton144 <anton-paramonov2000@yandex.ru>
2019-10-07 16:21:25 +0300

    Tmp client test

--- src/test/kotlin/ru/snailmail/backend/ClientTest.kt
+++ src/test/kotlin/ru/snailmail/backend/ClientTest.kt
@@ -1,4 +1,11 @@
 package ru.snailmail.backend
 
+import org.junit.jupiter.api.Test
+
 class ClientTest {
+    @Test
+    fun testRegister() {
+        Client.register("Anton", "best password")
+        Master.getUsers
+    }
 }
