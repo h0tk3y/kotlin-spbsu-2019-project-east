6140dea9ff9335b25163261291e9bdd6492740b6
Anton144 <anton-paramonov2000@yandex.ru>
2019-10-07 17:03:58 +0300

    ClientTest in progress

--- src/test/kotlin/ru/snailmail/backend/ClientTest.kt
+++ src/test/kotlin/ru/snailmail/backend/ClientTest.kt
@@ -1,11 +1,33 @@
 package ru.snailmail.backend
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
+import java.lang.IllegalArgumentException
 
 class ClientTest {
     @Test
     fun testRegister() {
-        Client.register("Anton", "best password")
-        Master.getUsers
+        val client = Client()
+        Assertions.assertDoesNotThrow {client.register("Grisha", "my password")}
+        Assertions.assertTrue(Master.searchUser("Grisha").name == "Grisha" &&
+                Master.searchUser("Grisha").password == "my password")
+    }
+
+    @Test
+    fun testLogin() {
+        val client = Client()
+        client.register("Anton", "my password")
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            client.logIn("Anton", "not my password")}
+        try {
+            client.logIn("Anton", "not my password")
+        } catch (e: IllegalArgumentException) {
+            Assertions.assertEquals(e.message, "Wrong password")
+        }
+    }
+
+    @Test
+    fun testSendMessage() {
+        //TODO
     }
 }
