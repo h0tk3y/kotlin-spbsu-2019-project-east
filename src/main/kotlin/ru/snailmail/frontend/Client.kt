486ced353053c237ec3aaef1c28dcb77f39abe7c
Chernogorsky Fedor <fyodorchernogor@gmail.com>
2019-10-21 16:26:02 +0300

    add http interaction

--- src/main/kotlin/ru/snailmail/frontend/Client.kt
+++ src/main/kotlin/ru/snailmail/frontend/Client.kt
@@ -2,11 +2,25 @@ package ru.snailmail.frontend
 import ru.snailmail.backend.*
 
 import io.ktor.auth.UserPasswordCredential
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 class Client {
     // TODO: request Master using network
     lateinit var user: User
         private set
+
+    fun greetings() : String?{
+        return sendGetRequest("")
+    }
+
+    //remove later
+    fun getUsers() : String?{
+        return sendGetRequest("users")
+    }
 
     fun register(creds: UserPasswordCredential) {
         Master.register(creds)
@@ -39,5 +53,39 @@ class Client {
 
     fun inviteUser(c: PublicChat, user: User) {
         Master.inviteUser(this.user, c, user)
+    }
+
+    private fun sendGetRequest(param: String) : String? {
+
+//        var reqParam = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(userName, "UTF-8")
+//        reqParam += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")
+        var reqParam = URLEncoder.encode(param, "UTF-8")
+
+        val mURL = URL("http://127.0.0.1:8080/$reqParam")
+
+        lateinit
+        var response:String;
+
+        with(mURL.openConnection() as HttpURLConnection) {
+            // optional default is GET
+            requestMethod = "GET"
+
+//            println("URL : $url")
+//            println("Response Code : $responseCode")
+
+            BufferedReader(InputStreamReader(inputStream)).use {
+                val res = StringBuffer()
+
+                var inputLine = it.readLine()
+                while (inputLine != null) {
+                    res.append(inputLine)
+                    inputLine = it.readLine()
+                }
+                it.close()
+//                println("Response : $res")
+                response = res.toString()
+            }
+        }
+        return response
     }
 }
