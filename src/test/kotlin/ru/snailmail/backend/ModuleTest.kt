e711961d252272ea5ec3c0dba39a1127bf6d23b7
Anton144 <anton-paramonov2000@yandex.ru>
2019-10-25 01:02:24 +0300

    deleted unnecessary commits

--- src/test/kotlin/ru/snailmail/backend/ModuleTest.kt
+++ src/test/kotlin/ru/snailmail/backend/ModuleTest.kt
@@ -3,16 +3,11 @@ package ru.snailmail.backend
 import com.google.gson.Gson
 import io.ktor.application.Application
 import io.ktor.auth.UserPasswordCredential
-import io.ktor.client.request.forms.formData
 import io.ktor.http.*
-import io.ktor.request.contentType
-import io.ktor.request.httpMethod
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 import io.ktor.server.testing.*
-import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.assertThrows
 
 
 class ModuleTest {
