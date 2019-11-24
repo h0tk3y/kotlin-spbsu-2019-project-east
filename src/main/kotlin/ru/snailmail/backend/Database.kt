b8b39a22bd70a960a124b4e6fcd8dd41ca5f3fca
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2019-11-24 21:08:10 +0300

    add database methods

--- src/main/kotlin/ru/snailmail/backend/Database.kt
+++ src/main/kotlin/ru/snailmail/backend/Database.kt
@@ -1,11 +1,10 @@
 package ru.snailmail.backend
 
-import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.*
 
 object Users : Table() {
     val userId = long("id").primaryKey()
-    val name = varchar("name", length = 50)
+    val name = varchar("name", length = 50).primaryKey()
     val password = varchar("password", length = 50)
 }
 
@@ -22,8 +21,8 @@ object Chats : Table() {
 }
 
 object ChatsToUsers : Table() {
-    val userId = long("userId").primaryKey()
-    val chatId = long("chatId").primaryKey()
+    val userId = long("userId")
+    val chatId = long("chatId")
 }
 
 object Messages : Table() {
@@ -34,14 +33,31 @@ object Messages : Table() {
 }
 
 object MessagesToChats : Table() {
-    val chatId = long("chatId").primaryKey()
-    val messageId = long("messageId").primaryKey()
+    val chatId = long("chatId")
+    val messageId = long("messageId")
 }
 
-fun findUserIdByLogin(t: Transaction, userLogin: String): UID? = TODO()
+fun findUserIdByLogin(t: Transaction, userLogin: String): UID? {
+    Users.select { Users.name eq userLogin }.firstOrNull()?.let {
+        return UID(it[Users.userId])
+    }
+    return null
+}
 
-fun addUser(t: Transaction, id: Long, login: String, password: String): Unit = TODO()
+fun addUser(t: Transaction, id: Long, userLogin: String, userPassword: String) {
+    Users.insert {
+        it[userId] = id
+        it[name] = userLogin
+        it[password] = userPassword
+    }
+}
 
-fun findUserById(t: Transaction, id: UID): User? = TODO()
+fun findUserById(t: Transaction, id: UID): User? {
+    Users.select { Users.userId eq id.id }.firstOrNull()?.let {
+        return User(it[Users.name], it[Users.password], id)
+    }
+    return null
+}
 
-fun userInChat(t: Transaction, userId: UID, chatId: UID): Boolean = TODO()
+fun userInChat(t: Transaction, userId: UID, chatId: UID): Boolean =
+    ChatsToUsers.select { (ChatsToUsers.chatId eq chatId.id) and (ChatsToUsers.userId eq userId.id) }.empty()
