3346031de816c5420764f505907f338fb63c8552
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2019-12-12 14:49:07 +0300

    fix createLichka test

--- src/test/kotlin/ru/snailmail/backend/ModuleTest.kt
+++ src/test/kotlin/ru/snailmail/backend/ModuleTest.kt
@@ -10,13 +10,13 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.resolveResource
 import io.ktor.server.testing.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 
@@ -43,6 +43,18 @@ class ModuleTest {
             addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
             setBody(Gson().toJson(cred))
         }
+    }
+
+    private fun handleRequestPostWithToken(engine: TestApplicationEngine, uri: String, token: String, request: Request): TestApplicationCall {
+        return engine.handleRequest(HttpMethod.Post, uri) {
+            addHeader(HttpHeaders.Authorization, "Bearer $token")
+            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            setBody(Gson().toJson(request))
+        }
+    }
+
+    private fun idByResponse(response: TestApplicationResponse): UID {
+        return jacksonObjectMapper().readValue(response.content.toString())
     }
 
     @Test
@@ -95,60 +107,32 @@ class ModuleTest {
         )
         val callLogin = handleRequestPost(this, "/login", cred)
         assertEquals(
-            userIdByResponse(callRegister.response).id,
-            JwtConfig.verifier.verify(callLogin.response.content).subject.toLong()
+            idByResponse(callRegister.response).id,
+            JwtConfig.verifier.verify(callLogin.response.content?.split('$')!![0]).subject.toLong()
         )
     }
 
-    private fun userIdByResponse(response: TestApplicationResponse): UID {
-        return jacksonObjectMapper().readValue(response.content.toString())
-    }
-
-    private fun lichkaIdByResponse(response: TestApplicationResponse): UID {
-        return jacksonObjectMapper().readValue(response.content.toString())
-    }
-
-    @Disabled
     @Test
     fun testCreateLichka() = withTestApplication(Application::module) {
         val cred1 = UserPasswordCredential("Anton1", "password")
-        val registerUser1 = handleRequest(HttpMethod.Post, "/register") {
-            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-            setBody(Gson().toJson(cred1))
-        }
-        val user1Id = userIdByResponse(registerUser1.response)
+        val registerUser1 = handleRequestPost(this, "/register", cred1)
+        val user1Id = idByResponse(registerUser1.response)
 
         val cred2 = UserPasswordCredential("Anton2", "password")
-        val registerUser2 = handleRequest(HttpMethod.Post, "/register") {
-            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-            setBody(Gson().toJson(cred2))
-        }
+        val registerUser2 = handleRequestPost(this, "/register", cred2)
+        val user2Id = idByResponse(registerUser2.response)
 
         val cred3 = UserPasswordCredential("Anton3", "password")
-        val registerUser3 = handleRequest(HttpMethod.Post, "/register") {
-            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-            setBody(Gson().toJson(cred3))
-        }
-        val user3Id = userIdByResponse(registerUser3.response)
+        val registerUser3 = handleRequestPost(this, "/register", cred3)
+        val user3Id = idByResponse(registerUser3.response)
 
-        val user2Id = userIdByResponse(registerUser2.response)
-        val token = handleRequest(HttpMethod.Post, "/login") {
-            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-            setBody(Gson().toJson(cred1))
-        }
-        val params = CreateLichkaRequest(user2Id)
-        val createLichka = handleRequest(HttpMethod.Post, "/createLichka") {
-            addHeader(HttpHeaders.Authorization, "Bearer ${token.response.content}")
-            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-            setBody(Gson().toJson(params))
-        }
-        val lichka1Id = lichkaIdByResponse(createLichka.response)
-        assertEquals(HttpStatusCode.OK, createLichka.response.status())
-        assertNotNull(Master.findChatById(lichka1Id))
-        val lichka1 = Master.findChatById(lichka1Id)
-        /*assert(Master.findUserById(user1Id)?.chats?.contains(lichka1) ?: false)
-        assert(Master.findUserById(user2Id)?.chats?.contains(lichka1) ?: false)
-        assert(!(Master.findUserById(user3Id)?.chats?.contains(lichka1) ?: true))*/
+        val token = handleRequestPost(this, "/login", cred1).response.content?.split('$')!![0]
+        val createLichkaWithUser2 = handleRequestPostWithToken(this, "/createLichka", token, CreateLichkaRequest(user2Id))
+        val createLichkaWithUser3 = handleRequestPostWithToken(this, "/createLichka", token, CreateLichkaRequest(user3Id))
+
+        assertEquals(HttpStatusCode.OK, createLichkaWithUser2.response.status())
+        assertEquals(HttpStatusCode.OK, createLichkaWithUser3.response.status())
+        assertNotEquals(idByResponse(createLichkaWithUser2.response), idByResponse(createLichkaWithUser3.response))
+
     }
-
 }
